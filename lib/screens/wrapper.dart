59508291a06932355290f663d4b91ed71200fb9a
Yash Kambaria <yashkambaria245@gmail.com>
2025-08-09 17:00:23 +0530

    Wrapper file added

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:coffee_brew/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //return home or auth
+    return Home();
+  }
+}
